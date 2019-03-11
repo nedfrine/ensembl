82ba61dc06cdbd52ccbe691dbe246faa9dfda010
Ivan <nithin.ivan@sap.com>
2019-03-11 08:13:48 +0530

    added error-handling for missing values and wrong params

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import flask
 import json
-
 import os
-
 from flask import render_template
 from flask import request
 from flask import Response
@@ -13,7 +11,6 @@ from flask_sqlalchemy import SQLAlchemy
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "mysql://anonymous:@193.62.193.10/ensembl_website_90"
 
-#app = Flask(__name__)
 
 
 app = flask.Flask(__name__)
@@ -28,33 +25,42 @@ def home():
 
 @app.route('/v1/gene_suggest', methods=['GET','POST'])
 def gene_suggest():
-    #if request.form:
-        #print(request.form)
     in_species = request.args.get('species')
     partial = request.args.get('query')
     in_limit = request.args.get('limit') or 10
+    if(in_species == '' or partial == '' or in_limit == ''):
+        raise InvalidUsage('Values missing', status_code=410)
     match_string = partial + '%'
     gene = gene_autocomplete.query.filter(and_(gene_autocomplete.species == in_species,gene_autocomplete.display_label.like(match_string))).limit(in_limit).all()
-    #print gene 
     return flask.jsonify(eqtls=[e.serialize() for e in gene]) 
-    #return "|".join(gene)
 
 
 @app.route('/v1/gene_species', methods=['GET','POST'])
 def gene_species():
-    #if request.form:
-        #print(request.form)
     in_species = request.args.get('species')
     gene_spec = gene_autocomplete.query.filter(gene_autocomplete.species == in_species).all()
-    #print gene_spec
     return flask.jsonify(eqtls=[e.serialize() for e in gene_spec])
 
+class InvalidUsage(Exception):
+    status_code = 400
 
-#@app.route('/genes', methods=['GET'])
-#def genes():
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
 
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
-
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = flask.jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 class gene_autocomplete(db.Model):
     display_label = db.Column(db.String(128), unique=False, nullable=True, primary_key=False)
